84b3995012e0d7bd2849e7d56f71e9917bc52f03
binggang.niu <binggang.niu@tadu.com>
2020-04-03 19:39:23 +0800

    feat：项目初始化

---
+++ README.md
@@ -0,0 +1,8 @@
+# 自动化部署脚手架
+
+**模块插件**
+chalk、
+commander、
+download-git-repo、
+inquirer、
+ora
---
+++ src/downLoad.js
@@ -0,0 +1,19 @@
+// git包
+const downLoad = require('download-git-repo')
+// 动画
+const ora = require('ora')
+let url = 'niubg/-vue-cli3-demo'
+let clone = false
+let downGit = (name) => {
+    const spinner = ora('正在拉取模板...')
+    spinner.start()
+    downLoad(url, name, {
+        clone
+    }, err => {
+        spinner.stop()
+        console.log(err?err:"项目创建成功")
+        process.exit(1)
+    })
+
+}
+module.exports = downGit
